52c0e79aeebb11dcc720b7335e8256766bfc3614
André Mendes <andregmendes@gmail.com>
2023-06-06 00:11:03 +0100

    Fuck yeah!

--- src/main/java/guru/springframework/sfgdi/services/DogPetService.java
+++ src/main/java/guru/springframework/sfgdi/services/DogPetService.java
@@ -1,15 +1,18 @@
 package guru.springframework.sfgdi.services;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 /**
  * Created by jt on 12/28/19.
  */
-@Profile({"dog", "default"})
 @Service
+@Profile("dog")
+/*@Primary*/
 public class DogPetService implements PetService {
     public String getPetType(){
         return "Dogs are the best!";
     }
 }
+
