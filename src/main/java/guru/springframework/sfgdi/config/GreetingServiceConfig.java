0c59a57c90757db985a12a208b4dead413141f9b
André Mendes <andregmendes@gmail.com>
2023-06-15 15:57:58 +0100

    Added Repository

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -1,6 +1,9 @@
 package guru.springframework.sfgdi.config;
 
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepositoryImpl;
 import guru.springframework.sfgdi.services.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -31,10 +34,15 @@ public class GreetingServiceConfig {
     }
 
 
+    @Bean
+    EnglishGreetingRepository englishGreetingRepository () {
+        return new EnglishGreetingRepositoryImpl();
+    };
+
     @Bean("i18nService")
     @Profile("EN")
-    I18nEnglishGreetingService greetingInEnglish() {
-        return new I18nEnglishGreetingService();
+    I18nEnglishGreetingService greetingInEnglish(EnglishGreetingRepository englishGreetingRepository) {
+        return new I18nEnglishGreetingService(englishGreetingRepository);
     }
 
     @Bean("i18nService")
